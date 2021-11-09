78cfdb9890f5cf528b2aef3d882612d1d54c0c2d
EC2 Default User <ec2-user@ip-172-31-85-21.ec2.internal>
2021-11-09 17:20:02 +0000

    [Finish]アクセス制限の実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,16 @@
 class BooksController < ApplicationController
 
   def create
-    book = Book.new(book_params)
-    book.user_id = current_user.id
-    book.save
-    redirect_to book_path(book), notice: "You have created book successfully."
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      p @book
+      redirect_to book_path(@book), notice: "You have created book successfully."
+    else
+      @user = current_user
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
@@ -19,12 +25,18 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user != current_user
+      redirect_to books_path
+    end
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id), notice: "You have updated book successfully."
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), notice: "You have updated book successfully."
+    else
+      render :edit
+    end
   end
 
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,12 +13,18 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to user_path(current_user)
+    end
   end
 
   def update
-    user = User.find(params[:id])
-    user.update(user_params)
-    redirect_to user_path(user.id), notice: "You have updated user successfully."
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user.id), notice: "You have updated user successfully."
+    else
+      render :edit
+    end
   end
 
   def destroy
